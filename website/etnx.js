fe93f2004ef700698af6302b1da50b68ee6e3c82
mutalisk999 <tangjian999999999@gmail.com>
2021-11-12 21:16:18 +0800

    first commit

---
+++ website/etnx.js
@@ -0,0 +1,21 @@
+var api = "//45.137.11.136:8117";
+var coinUnits = 1000000000000;
+var poolHost = "pool.electronero.org";
+var showStatus = true; // set to false if you want to hide the poolStatus
+// create poolStatus with HTML, or Text.
+var poolStatus = "<p>  11/11/2021: Mining is stable. </p>";
+
+var irc = "irc.freenode.net/#etnx-pools";
+
+var email = "support@support.etnx.pool";
+
+// var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+var cryptonatorWidget = [];
+
+var easyminerDownload = "#";
+
+var blockchainExplorer = "https://localmonero.co/blocks/block/{id}";
+
+var transactionExplorer = "https://localmonero.co/blocks/tx/{id}";
+
+var themeCss = "themes/default.css";
